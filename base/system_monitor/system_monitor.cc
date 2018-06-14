466510ff8370e9d1250f15a5331cc3e84be9fff3
Scott Graham <scottmg@chromium.org>
2018-06-14 11:33:13 -0700

    Remove vlog, valgrind, native_library, tuple, supports_user_data
    Change-Id: Ib0be4fd01c221612cc5195a1766a30ed2689f7e1

--- base/system_monitor/system_monitor.cc
+++ base/system_monitor/system_monitor.cc
@@ -43,7 +43,6 @@ void SystemMonitor::RemoveDevicesChangedObserver(DevicesChangedObserver* obs) {
 }
 
 void SystemMonitor::NotifyDevicesChanged(DeviceType device_type) {
-  DVLOG(1) << "DevicesChanged with device type " << device_type;
   devices_changed_observer_list_->Notify(
       FROM_HERE, &DevicesChangedObserver::OnDevicesChanged, device_type);
 }
