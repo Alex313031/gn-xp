782dcbff7f5f2bc7f41408752857e45941a91b98
Gary Miguel <garymm@google.com>
2018-09-17 12:48:17 -0700

    Stop referring to Python in exec_script.
    exec_script can now execute non-Python scripts.
    
    Change-Id: Ia48485c28036eb3a8cb3defffb556ad23a362dc2

--- tools/gn/function_exec_script.cc
+++ tools/gn/function_exec_script.cc
@@ -83,8 +83,8 @@ const char kExecScript_Help[] =
 Arguments:
 
   filename:
-      File name of python script to execute. Non-absolute names will be treated
-      as relative to the current build file.
+      File name of script to execute. Non-absolute names will be treated as
+      relative to the current build file.
 
   arguments:
       A list of strings to be passed to the script as arguments. May be
@@ -133,7 +133,7 @@ Value RunExecScript(Scope* scope,
   if (!CheckExecScriptPermissions(build_settings, function, err))
     return Value();
 
-  // Find the python script to run.
+  // Find the script to run.
   std::string script_source_path = cur_dir.ResolveRelativeAs(
       true, args[0], err,
       scope->settings()->build_settings()->root_path_utf8());
@@ -173,8 +173,8 @@ Value RunExecScript(Scope* scope,
   }
 
   // Make the command line.
-  const base::FilePath& python_path = build_settings->python_path();
-  base::CommandLine cmdline(python_path);
+  const base::FilePath& interpreter_path = build_settings->python_path();
+  base::CommandLine cmdline(interpreter_path);
 
   // CommandLine tries to interpret arguments by default.  Disable that so
   // that the arguments will be passed through exactly as specified.
@@ -199,10 +199,10 @@ Value RunExecScript(Scope* scope,
   Ticks begin_exec = 0;
   if (g_scheduler->verbose_logging()) {
 #if defined(OS_WIN)
-    g_scheduler->Log("Pythoning",
+    g_scheduler->Log("Executing",
                      base::UTF16ToUTF8(cmdline.GetCommandLineString()));
 #else
-    g_scheduler->Log("Pythoning", cmdline.GetCommandLineString());
+    g_scheduler->Log("Executing", cmdline.GetCommandLineString());
 #endif
     begin_exec = TicksNow();
   }
@@ -227,14 +227,15 @@ Value RunExecScript(Scope* scope,
     if (!internal::ExecProcess(cmdline, startup_dir, &output, &stderr_output,
                                &exit_code)) {
       *err = Err(
-          function->function(), "Could not execute python.",
-          "I was trying to execute \"" + FilePathToUTF8(python_path) + "\".");
+          function->function(), "Could not execute interpreter.",
+          "I was trying to execute \"" + FilePathToUTF8(interpreter_path) +
+          "\".");
       return Value();
     }
   }
   if (g_scheduler->verbose_logging()) {
     g_scheduler->Log(
-        "Pythoning",
+        "Executing",
         script_source_path + " took " +
             base::Int64ToString(
                 TicksDelta(TicksNow(), begin_exec).InMilliseconds()) +
