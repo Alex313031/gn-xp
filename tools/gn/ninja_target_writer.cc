afd6a0423d0390c74603854c29f93a59dd6ec9a2
Julie Hockett <juliehockett@google.com>
2018-10-19 09:44:06 -0700

    [ninja_target_writer] Check stamp file output
    Adds a check to confirm that the build is not trying to write a stamp
    file that depends on itself.
    
    Bug: crbug.com/gn/16
    Change-Id: Iaee1c6e27d273b02b476fbb515c4ee0ae0bc074e

--- tools/gn/ninja_target_writer.cc
+++ tools/gn/ninja_target_writer.cc
@@ -26,6 +26,9 @@
 #include "tools/gn/target.h"
 #include "tools/gn/trace.h"
 
+#include "tools/gn/standard_out.h"
+
+
 NinjaTargetWriter::NinjaTargetWriter(const Target* target, std::ostream& out)
     : settings_(target->settings()),
       target_(target),
@@ -308,6 +311,19 @@ void NinjaTargetWriter::WriteStampForTarget(
                        base::CompareCase::INSENSITIVE_ASCII))
       << "Output should end in \".stamp\" for stamp file output. Instead got: "
       << "\"" << stamp_file.value() << "\"";
+
+  // Then validate that this stamp file is not something the target expects to
+  // depend on.
+  for (const auto& file : files)
+    DCHECK(file.value() != stamp_file.value())
+        << "Circular output in file dependency: " << target_->label().name()
+        << " is trying to generate the stamp file " << file.value()
+        << " that already exists.\"";
+  for (const auto& file : order_only_deps)
+    DCHECK(file.value() != stamp_file.value())
+        << "Circular output in order-only dependency: "
+        << target_->label().name() << " is trying to generate the stamp file "
+        << file.value() << " that already exists.\"";
 
   out_ << "build ";
   path_output_.WriteFile(out_, stamp_file);
