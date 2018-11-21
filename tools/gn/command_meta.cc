c9d913bac0701c50a7b3184d3e76036ea8e74df8
Julie Hockett <juliehockett@google.com>
2018-11-21 13:10:21 -0800

    [metadata] Adding `meta` command
    Change-Id: Ib2d436b647936cca989e0d14f387c1383ad5df57

---
+++ tools/gn/command_meta.cc
@@ -0,0 +1,132 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <algorithm>
+#include <set>
+
+#include "base/command_line.h"
+#include "base/strings/string_split.h"
+#include "tools/gn/commands.h"
+#include "tools/gn/metadata_walk.h"
+#include "tools/gn/setup.h"
+#include "tools/gn/standard_out.h"
+#include "tools/gn/switches.h"
+#include "tools/gn/target.h"
+
+namespace commands {
+
+const char kMeta[] = "meta";
+const char kMeta_HelpShort[] = "meta: List target metadata collection results.";
+const char kMeta_Help[] =
+    R"(gn meta <out_dir> <target>* --data=<key>[,<key>*]* [--walk=<key>[,<key>*]*]
+       [--rebase]
+
+  Lists collected metaresults of all given targets for the given data key(s),
+  collecting metadata dependencies as specified by the givn walk key(s).
+
+Examples
+
+  gn meta out/Debug \"//base/foo\" --data=files
+      Lists collected metaresults for the `files` key in the //base/foo:foo
+      target and all of its dependency tree.
+
+  gn meta out/Debug \"//base/foo\" --data=files --data=other
+      Lists collected metaresults for the `files` and `other` keys in the
+      //base/foo:foo target and all of its dependency tree.
+
+  gn meta out/Debug \"//base/foo\" --data=files --walk=stop
+      Lists collected metaresults for the `files` key in the //base/foo:foo
+      target and all of the dependencies listed in the `stop` key (and so on).
+
+  gn meta out/Debug \"//base/foo\" --data=files --rebase-files
+      Lists collected metaresults for the `files` key in the //base/foo:foo
+      target and all of its dependency tree, rebasing the strings in the `files`
+      key onto the source directory of the target's declaration.
+)";
+
+int RunMeta(const std::vector<std::string>& args) {
+  if (args.size() == 0) {
+    Err(Location(), "You're holding it wrong.",
+        "Usage: \"gn meta <out_dir> <target>* --data=<key>[,<key>*] "
+        "[--walk=<key>[,<key>*]*] [--rebase-files]\"")
+        .PrintToStdout();
+    return 1;
+  }
+
+  Setup* setup = new Setup;
+  if (!setup->DoSetup(args[0], false) || !setup->Run())
+    return 1;
+
+  const base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
+  bool rebase_files = cmdline->HasSwitch(switches::kMetaRebaseFiles);
+  std::string data_keys_str =
+      cmdline->GetSwitchValueASCII(switches::kMetaDataKeys);
+  std::string walk_keys_str =
+      cmdline->GetSwitchValueASCII(switches::kMetaWalkKeys);
+
+  std::vector<std::string> inputs(args.begin() + 1, args.end());
+
+  UniqueVector<const Target*> targets;
+  for (const auto& input : inputs) {
+    const Target* target = ResolveTargetFromCommandLineString(setup, input);
+    if (!target) {
+      Err(Location(), "Unknown target " + input).PrintToStdout();
+      return 1;
+    }
+    targets.push_back(target);
+  }
+
+  std::vector<base::StringPiece> data_keys = base::SplitStringPiece(
+      data_keys_str, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  if (data_keys.empty()) {
+    return 1;
+  }
+  std::vector<base::StringPiece> walk_keys = base::SplitStringPiece(
+      walk_keys_str, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
+  Err err;
+  std::vector<Value> result =
+      WalkMetadata(targets, data_keys, walk_keys, rebase_files, &err);
+  if (err.has_error()) {
+    err.PrintToStdout();
+    return 1;
+  }
+
+  OutputString("Metadata values\n", DECORATION_DIM);
+  for (const auto& value : result)
+    OutputString("  - " + value.ToString(false) + "\n");
+  OutputString("\nExtracted from:\n", DECORATION_DIM);
+  bool first = true;
+  for (const auto* target : targets) {
+    if (!first) {
+      first = false;
+      OutputString(", ", DECORATION_DIM);
+    }
+    OutputString("  - " + target->label().GetUserVisibleName(false) + "\n",
+                 DECORATION_DIM);
+  }
+  OutputString("\nusing data keys:\n", DECORATION_DIM);
+  first = true;
+  for (const auto& key : data_keys) {
+    if (!first) {
+      first = false;
+      OutputString(", ", DECORATION_DIM);
+    }
+    OutputString("  - " + key.as_string() + "\n", DECORATION_DIM);
+  }
+  if (!walk_keys.empty()) {
+    OutputString("\nand using walk keys:\n", DECORATION_DIM);
+    first = true;
+    for (const auto& key : walk_keys) {
+      if (!first) {
+        first = false;
+        OutputString(", ", DECORATION_DIM);
+      }
+      OutputString("  - " + key.as_string() + "\n", DECORATION_DIM);
+    }
+  }
+  OutputString("\n");
+  return 0;
+}
+
+}  // namespace commands
