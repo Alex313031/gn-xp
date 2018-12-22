9f095f42fae012ff979bb7c0dd61b4d01bc1974a
Brett Wilson <brettw@chromium.org>
2018-12-21 21:20:05 -0800

    Improve help output.
    Make the links in markdown for the "args" command and target variable
    work by disambiguating href names in by variable type.
    
    Special-case "gn help args" on the command line by printing help for
    both the command and the variable.
    
    Fix misnaming of xcode_test_application_name.
    
    Simplify markdown tag emitting by requiring the the caller generate
    a tag for linking rather than trying to autogenerate one based on
    text that comes before a colon. This autogeneration code was not
    used.
    
    Change-Id: I7dd3f806eac5afaef746b2e37bf0903d9ae72b14

--- tools/gn/standard_out.cc
+++ tools/gn/standard_out.cc
@@ -78,8 +78,8 @@ void WriteToStdOut(const std::string& output) {
 #endif  // !defined(OS_WIN)
 
 void OutputMarkdownDec(TextDecoration dec) {
-  // The markdown rendering turns "dim" text to italics and any
-  // other colored text to bold.
+// The markdown rendering turns "dim" text to italics and any
+// other colored text to bold.
 
 #if defined(OS_WIN)
   DWORD written = 0;
@@ -226,21 +226,24 @@ void PrintSectionHelp(const std::string& line,
   }
 }
 
-void PrintShortHelp(const std::string& line) {
+void PrintShortHelp(const std::string& line, const std::string& link_tag) {
   EnsureInitialized();
 
   size_t colon_offset = line.find(':');
   size_t first_normal = 0;
   if (colon_offset != std::string::npos) {
     if (is_markdown) {
-      OutputString("    *   [" + line + "](#" + line.substr(0, colon_offset) +
-                   ")\n");
+      if (link_tag.empty())
+        OutputString("    *   " + line + "\n");
+      else
+        OutputString("    *   [" + line + "](#" + link_tag + ")\n");
     } else {
       OutputString("  " + line.substr(0, colon_offset), DECORATION_YELLOW);
       first_normal = colon_offset;
     }
   } else if (is_markdown) {
-    OutputString("    *   [" + line + "](" + line + ")\n");
+    const std::string& the_tag = link_tag.empty() ? line : link_tag;
+    OutputString("    *   [" + line + "](#" + the_tag + ")\n");
   }
 
   if (is_markdown)
@@ -289,16 +292,8 @@ void PrintLongHelp(const std::string& text, const std::string& tag) {
           in_body = false;
         }
 
-        if (first_header) {
-          std::string the_tag = tag;
-          if (the_tag.size() == 0) {
-            if (line.substr(0, 2) == "gn") {
-              the_tag = line.substr(3, line.substr(3).find(' '));
-            } else {
-              the_tag = line.substr(0, line.find(':'));
-            }
-          }
-          OutputString("### <a name=\"" + the_tag + "\"></a>", DECORATION_NONE,
+        if (first_header && !tag.empty()) {
+          OutputString("### <a name=\"" + tag + "\"></a>", DECORATION_NONE,
                        NO_ESCAPING);
           first_header = false;
         } else {
