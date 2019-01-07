9c87133f00c344ebabaa617f2cf3dab47ccace09
Scott Graham <scottmg@chromium.org>
2019-01-07 13:56:49 -0800

    format: Fix incorrect dewrap of `if`
    When `if (...stuff...) {` was exactly 81 characters, it would be
    incorrectly wrapped because the trailing { wasn't being taken into
    account in penalizing single line formatting.
    
    To avoid this, pass the full suffix into the child formatter.
    
    Bug: gn:36
    
    Change-Id: I5af9a62a3de4953ea0d60a85bd558281410a8f09

--- tools/gn/command_format.cc
+++ tools/gn/command_format.cc
@@ -112,6 +112,7 @@ class Printer {
   enum SequenceStyle {
     kSequenceStyleList,
     kSequenceStyleBracedBlock,
+    kSequenceStyleBracedBlockAlreadyOpen,
   };
 
   struct Metrics {
@@ -718,10 +719,10 @@ int Printer::Expr(const ParseNode* root,
              false);
   } else if (const ConditionNode* condition = root->AsConditionNode()) {
     Print("if (");
-    // TODO(scottmg): The { needs to be included in the suffix here.
-    Expr(condition->condition(), kPrecedenceLowest, ") ");
-    Sequence(kSequenceStyleBracedBlock, condition->if_true()->statements(),
-             condition->if_true()->End(), false);
+    Expr(condition->condition(), kPrecedenceLowest, ") {");
+    Sequence(kSequenceStyleBracedBlockAlreadyOpen,
+             condition->if_true()->statements(), condition->if_true()->End(),
+             false);
     if (condition->if_false()) {
       Print(" else ");
       // If it's a block it's a bare 'else', otherwise it's an 'else if'. See
@@ -778,10 +779,13 @@ void Printer::Sequence(SequenceStyle style,
                        const std::vector<std::unique_ptr<PARSENODE>>& list,
                        const ParseNode* end,
                        bool force_multiline) {
-  if (style == kSequenceStyleList)
+  if (style == kSequenceStyleList) {
     Print("[");
-  else if (style == kSequenceStyleBracedBlock)
+  } else if (style == kSequenceStyleBracedBlock) {
     Print("{");
+  } else if (style == kSequenceStyleBracedBlockAlreadyOpen) {
+    style = kSequenceStyleBracedBlock;
+  }
 
   if (style == kSequenceStyleBracedBlock) {
     force_multiline = true;
