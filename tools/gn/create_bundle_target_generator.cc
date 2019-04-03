1bd96507f4830625698f59929b40869282079f7c
Sylvain Defresne <sdefresne@chromium.org>
2019-04-03 16:24:09 +0200

    Fix issues with bundle_data/create_bundle targets
    Remove the bundle_plugins_dir variable as it is not really required
    (the value is always the same for both iOS and macOS) and the value
    can be inlined instead ({{bundle_contents_dir}}/Plugins).
    
    Fix NinjaCreateBundleTargetWriter to only complain about missing
    compile_xcassets tool if the target require it (i.e. if it defines
    a list of resources to compile in an asset catalog).
    
    Fix incorrect use of deps instead of public_deps in bundle_data
    documentation.
    
    Bug: https://bugs.chromium.org/p/gn/issues/detail?id=64
    Change-Id: I28a0a3d673dc1b39804c466499e6e91f034a2113

--- tools/gn/create_bundle_target_generator.cc
+++ tools/gn/create_bundle_target_generator.cc
@@ -7,6 +7,7 @@
 #include <map>
 
 #include "base/logging.h"
+#include "base/strings/stringprintf.h"
 #include "tools/gn/filesystem_utils.h"
 #include "tools/gn/label_pattern.h"
 #include "tools/gn/parse_tree.h"
@@ -31,20 +32,21 @@ void CreateBundleTargetGenerator::DoRun() {
 
   BundleData& bundle_data = target_->bundle_data();
   if (!FillBundleDir(SourceDir(), variables::kBundleRootDir,
-                     &bundle_data.root_dir()))
+                     &bundle_data.root_dir())) {
     return;
+  }
   if (!FillBundleDir(bundle_data.root_dir(), variables::kBundleContentsDir,
-                     &bundle_data.contents_dir()))
+                     &bundle_data.contents_dir())) {
     return;
+  }
   if (!FillBundleDir(bundle_data.root_dir(), variables::kBundleResourcesDir,
-                     &bundle_data.resources_dir()))
+                     &bundle_data.resources_dir())) {
     return;
+  }
   if (!FillBundleDir(bundle_data.root_dir(), variables::kBundleExecutableDir,
-                     &bundle_data.executable_dir()))
+                     &bundle_data.executable_dir())) {
     return;
-  if (!FillBundleDir(bundle_data.root_dir(), variables::kBundlePlugInsDir,
-                     &bundle_data.plugins_dir()))
-    return;
+  }
 
   if (!FillXcodeExtraAttributes())
     return;
