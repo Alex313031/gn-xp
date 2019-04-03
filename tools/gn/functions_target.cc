841eb9a7cc6c2654256ce3ea2d3fbd9c721332cb
Sylvain Defresne <sdefresne@chromium.org>
2019-04-03 14:10:14 +0200

    Fix issues with bundle_data/create_bundle targets
    Remove the bundle_plugins_dir variable as it is not really required
    (the value is always the same for both iOS and macOS) and the value
    can be inlined instead ({{bundle_contents_dir}}/Plugins).
    
    Fix NinjaCreateBundleTargetWriter to only complain about missing
    compile_xcassets tool if the target require it (i.e. if it defines
    a list of resources to compile in an asset catalog).
    
    Fix CreateBundleTargetGenerator to correctly report an error if a
    required variable is not set and changed bundle_executable_dir to
    not be required (not all bundle contains a binary).
    
    Fix incorrect use of deps instead of public_deps in bundle_data
    documentation.
    
    Bug: 64
    Change-Id: I28a0a3d673dc1b39804c466499e6e91f034a2113

--- tools/gn/functions_target.cc
+++ tools/gn/functions_target.cc
@@ -359,11 +359,10 @@ Code signing
 Variables
 
   bundle_root_dir*, bundle_contents_dir*, bundle_resources_dir*,
-  bundle_executable_dir*, bundle_plugins_dir*, bundle_deps_filter, deps,
-  data_deps, public_deps, visibility, product_type, code_signing_args,
-  code_signing_script, code_signing_sources, code_signing_outputs,
-  xcode_extra_attributes, xcode_test_application_name, partial_info_plist,
-  metadata
+  bundle_executable_dir, bundle_deps_filter, deps, data_deps, public_deps,
+  visibility, product_type, code_signing_args, code_signing_script,
+  code_signing_sources, code_signing_outputs, xcode_extra_attributes,
+  xcode_test_application_name, partial_info_plist, metadata
   * = required
 
 Example
@@ -389,7 +388,7 @@ Example
       }
 
       bundle_data("${app_name}_bundle_info_plist") {
-        deps = [ ":${app_name}_generate_info_plist" ]
+        public_deps = [ ":${app_name}_generate_info_plist" ]
         sources = [ "$gen_path/Info.plist" ]
         outputs = [ "{{bundle_contents_dir}}/Info.plist" ]
       }
@@ -406,9 +405,9 @@ Example
       code_signing =
           defined(invoker.code_signing) && invoker.code_signing
 
-      if (is_ios && !code_signing) {
+      if (!is_ios || !code_signing) {
         bundle_data("${app_name}_bundle_executable") {
-          deps = [ ":${app_name}_generate_executable" ]
+          public_deps = [ ":${app_name}_generate_executable" ]
           sources = [ "$gen_path/$app_name" ]
           outputs = [ "{{bundle_executable_dir}}/$app_name" ]
         }
@@ -422,7 +421,6 @@ Example
           bundle_contents_dir = bundle_root_dir
           bundle_resources_dir = bundle_contents_dir
           bundle_executable_dir = bundle_contents_dir
-          bundle_plugins_dir = "${bundle_contents_dir}/Plugins"
 
           extra_attributes = {
             ONLY_ACTIVE_ARCH = "YES"
@@ -433,7 +431,6 @@ Example
           bundle_contents_dir  = "${bundle_root_dir}/Contents"
           bundle_resources_dir = "${bundle_contents_dir}/Resources"
           bundle_executable_dir = "${bundle_contents_dir}/MacOS"
-          bundle_plugins_dir = "${bundle_contents_dir}/Plugins"
         }
         deps = [ ":${app_name}_bundle_info_plist" ]
         if (is_ios && code_signing) {
