70a83f7d56de4b8dbc55d40a43d56f4770ad4f49
Nico Weber <thakis@chromium.org>
2019-11-07 15:30:04 -0500

    Enable -Wall -Wextra compiler warnings on non-Win too.
    Change-Id: I2a9b0e744917405e863cea5fa9d458f3084fb302

--- src/base/files/file_util_posix.cc
+++ src/base/files/file_util_posix.cc
@@ -142,19 +142,6 @@ bool DetermineDevShmExecutable() {
 }
 #endif  // defined(OS_LINUX) || defined(OS_AIX)
 
-bool AdvanceEnumeratorWithStat(FileEnumerator* traversal,
-                               FilePath* out_next_path,
-                               struct stat* out_next_stat) {
-  DCHECK(out_next_path);
-  DCHECK(out_next_stat);
-  *out_next_path = traversal->Next();
-  if (out_next_path->empty())
-    return false;
-
-  *out_next_stat = traversal->GetInfo().stat();
-  return true;
-}
-
 bool CopyFileContents(File* infile, File* outfile) {
   static constexpr size_t kBufferSize = 32768;
   std::vector<char> buffer(kBufferSize);
