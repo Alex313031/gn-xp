02bdd2f060c1916e3d808d36f773a5aeabc5951a
Natalie Chouinard <chouinard@google.com>
2019-12-05 14:14:07 -0800

    [format] Sort java_files and expand sources sorting
    Sort all "java_files" lists using the same logic as "sources", and
    expand "sources" sorting to apply to any lists with the suffix
    "sources".
    
    May consider a more general solution in future,
    see: https://crbug.com/gn/134.
    
    Change-Id: I152d8d18448f056c9e78ced5653ce13ebb8e6f3b

--- src/gn/command_format.cc
+++ src/gn/command_format.cc
@@ -349,7 +349,10 @@ void Printer::SortIfSourcesOrDeps(const BinaryOpNode* binop) {
        binop->op().value() == "-=") &&
       ident && list) {
     const std::string_view lhs = ident->value().value();
-    if (lhs == "public" || lhs == "sources")
+    // TODO(crbug.com/gn/134): Support sorting other lists without hardcoding
+    // names here.
+    if (base::EndsWith(lhs, "sources", base::CompareCase::SENSITIVE) ||
+        lhs == "public" || lhs == "java_files")
       const_cast<ListNode*>(list)->SortAsStringsList();
     else if (lhs == "deps" || lhs == "public_deps")
       const_cast<ListNode*>(list)->SortAsDepsList();
