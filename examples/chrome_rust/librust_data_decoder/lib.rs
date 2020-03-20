7bfc0657db1d1d4cc1d7d55561dc80d30d15a73f
Benjamin Brittain <bwb@google.com>
2020-03-20 07:23:48 -0700

    WIP SAMPLE
    $ gn gen out/ && ninja -C out -v
    
    Change-Id: Iac11eb086906efcdd102148b1bed05f226192503

---
+++ examples/chrome_rust/librust_data_decoder/lib.rs
@@ -0,0 +1,15 @@
+extern crate third_party_dep;
+
+use std::convert::TryInto;
+
+extern "C" {
+    pub fn callback_into_c() -> i32;
+}
+
+#[no_mangle]
+pub fn rust_lib_c_thing(length: u64) -> bool {
+    let l: i32 = length.try_into().unwrap();
+    let _ = l * unsafe { callback_into_c() };
+    third_party_dep::footle(l);
+    true
+}
---
+++ examples/chrome_rust/third_party_dep/lib.rs
@@ -0,0 +1,5 @@
+
+pub fn footle(a: i32) -> i32 {
+    56 * a
+}
+
