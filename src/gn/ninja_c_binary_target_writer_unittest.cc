e6ae65177030fa3f0329c95b5b7954e5efedace6
Adrian Taylor <adetaylor@chromium.org>
2020-04-26 20:49:18 -0700

    Allow rlib linking into C++ targets.
    Previously, we assumed that the only way to link Rust rlibs into C++ was
    by having an intermediate Rust "static library" target, which is in
    effect a complete static library containing all the dependencies of that
    particular Rust code (including transitive dependencies and the standard
    library).
    
      [rlibs] -> [Rust staticlib] -> [solink/link etc.]
    
    But this doesn't work in cases where multiple Rust libraries may
    sometimes be linked in different permutations. In Chrome we have
    just that situation. In a debug build, Chrome exists as multiple
    .so files, whilst in a release build, it's linked into a single
    executable.
    
    Debug build:
      [rlib A] -> [Rust staticlib A] -> [solink A]
    
      [rlib B] -> [Rust staticlib B] -> [solink B]
    
    Release build:
      [rlib A] -> [Rust staticlib A] -\
                                      |
      [rlib B] -> [Rust staticlib B] -+--> [exe - violates ODR]
    
    The ODR violations in producing the final linked product assume
    there are bits of Rust stuff repeated across both staticlibs.
    e.g. bits of the Rust standard library. This would also happen
    if rlibs A and B both depended upon rlib C.
    
    Furthermore, these rlibs are so deeply nested in our dependency
    tree that they stand no realistic chance of determining whether
    the final product is a single binary or several binaries. gn does
    not encourage this kind of global knowledge anyway, and it feels wrong
    for a distant rlib to make decisions based upon the final output.
    
    Therefore, we have switched to an alternative strategy where Rust rlibs
    are linked directly into the final C++ executables and shared objects,
    together with the Rust standard library and other dependencies which
    are normally inserted by rustc as it calls the linker.
    
    In order to achieve this with gn, we need rlibs to be linked into C++
    targets.
    
    Instead of including them directly into the {{inputs}} for a C++
    link target, they're provided as a separate
    
      rlibs = ...
    
    substitution. This is because some toolchains may wish to treat them
    differently (for instance, not including them inside a
    -Wl,--whole-archive directive).
    
    This should have no effect on projects using the former strategy
    of linking rlibs together into a static library, because that static
    library should "absorb" its dependencies rather than passing them on
    to downstream C++ targets, and thus such dependencies should not be
    visible to the Ninja C target writer.
    
    An alternative would have been to place such rlibs within the existing
    
      libs =
    
    subtitution; however, such entries are typically of the form "-lfoo"
    and are assembled from config directives rather than OutputFile paths.
    
    This change doesn't deliberately change any behavior relating to
    Rust procedural macros. It seems questionable to have any C++ targets
    ever depend directly on procedural macros; further work is required
    here.
    
    Change-Id: I5fd81a872339e7334889fc3b73d346586163f0dc

--- src/gn/ninja_c_binary_target_writer_unittest.cc
+++ src/gn/ninja_c_binary_target_writer_unittest.cc
@@ -1326,12 +1326,36 @@ TEST_F(NinjaCBinaryTargetWriterTest, RustDeps) {
     library_target.SetToolchain(setup.toolchain());
     ASSERT_TRUE(library_target.OnResolved(&err));
 
+    Target rlib_target2(setup.settings(), Label(SourceDir("//qux/"), "lib2"));
+    rlib_target2.set_output_type(Target::RUST_LIBRARY);
+    rlib_target2.visibility().SetPublic();
+    SourceFile quxlib("//qux/lib.rs");
+    rlib_target2.sources().push_back(quxlib);
+    rlib_target2.source_types_used().Set(SourceFile::SOURCE_RS);
+    rlib_target2.rust_values().set_crate_root(quxlib);
+    rlib_target2.rust_values().crate_name() = "lib2";
+    rlib_target2.SetToolchain(setup.toolchain());
+    ASSERT_TRUE(rlib_target2.OnResolved(&err));
+
+    Target rlib_target3(setup.settings(), Label(SourceDir("//quux/"), "lib3"));
+    rlib_target3.set_output_type(Target::RUST_LIBRARY);
+    rlib_target3.visibility().SetPublic();
+    SourceFile quuxlib("//quux/lib.rs");
+    rlib_target3.sources().push_back(quuxlib);
+    rlib_target3.source_types_used().Set(SourceFile::SOURCE_RS);
+    rlib_target3.public_deps().push_back(LabelTargetPair(&rlib_target2));
+    rlib_target3.rust_values().set_crate_root(quuxlib);
+    rlib_target3.rust_values().crate_name() = "lib3";
+    rlib_target3.SetToolchain(setup.toolchain());
+    ASSERT_TRUE(rlib_target3.OnResolved(&err));
+
     Target target(setup.settings(), Label(SourceDir("//bar/"), "bar"));
     target.set_output_type(Target::EXECUTABLE);
     target.visibility().SetPublic();
     target.sources().push_back(SourceFile("//bar/bar.cc"));
     target.source_types_used().Set(SourceFile::SOURCE_CPP);
     target.private_deps().push_back(LabelTargetPair(&library_target));
+    target.private_deps().push_back(LabelTargetPair(&rlib_target3));
     target.SetToolchain(setup.toolchain());
     ASSERT_TRUE(target.OnResolved(&err));
 
@@ -1350,12 +1374,14 @@ TEST_F(NinjaCBinaryTargetWriterTest, RustDeps) {
         "\n"
         "build obj/bar/bar.bar.o: cxx ../../bar/bar.cc\n"
         "\n"
-        "build ./bar: link obj/bar/bar.bar.o obj/foo/libfoo.a\n"
+        "build ./bar: link obj/bar/bar.bar.o obj/foo/libfoo.a | "
+        "obj/quux/lib3.rlib obj/qux/lib2.rlib\n"
         "  ldflags =\n"
         "  libs =\n"
         "  frameworks =\n"
         "  output_extension = \n"
-        "  output_dir = \n";
+        "  output_dir = \n"
+        "  rlibs = obj/quux/lib3.rlib obj/qux/lib2.rlib\n";
 
     std::string out_str = out.str();
     EXPECT_EQ(expected, out_str) << expected << "\n" << out_str;
