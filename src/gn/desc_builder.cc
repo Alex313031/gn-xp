219364d13d8a3d0538f09a085b8758b6ac3605ef
Nathan Mulcahey <nmulcahey@google.com>
2020-07-09 17:27:47 -0700

    [rust] Fix `rustenv`
    A `rustenv` specified via config() is not merged in the target.
    
    Change-Id: Iabdf5e02d1bcc41696faab7a137fbb5bff405eb4

--- src/gn/desc_builder.cc
+++ src/gn/desc_builder.cc
@@ -484,6 +484,7 @@ class TargetDescBuilder : public BaseDescBuilder {
       CONFIG_VALUE_ARRAY_HANDLER(cflags_cc, std::string)
       CONFIG_VALUE_ARRAY_HANDLER(cflags_objc, std::string)
       CONFIG_VALUE_ARRAY_HANDLER(cflags_objcc, std::string)
+      CONFIG_VALUE_ARRAY_HANDLER(rustenv, std::string)
       CONFIG_VALUE_ARRAY_HANDLER(rustflags, std::string)
       CONFIG_VALUE_ARRAY_HANDLER(defines, std::string)
       CONFIG_VALUE_ARRAY_HANDLER(include_dirs, SourceDir)
