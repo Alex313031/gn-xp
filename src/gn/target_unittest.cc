2184e7ec1593313f09250e0265cbe83979d58b09
David 'Digit' Turner <digit@google.com>
2022-01-28 18:02:49 +0100

    Implement and enable 'no_stamp_files'
    This is the last CL to enable the new 'no_stamp_files'
    build setting flag, which enables the generation of Ninja
    phony targets instead of stamp files.
    
    - Modify the Ninja target writers to understand phony
      aliases.
    
    - Allow no_stamp_files to be set in the build settings
      file (e.g. `.gn`) without asserting. Note that when
      this is not set, this CL should not change the output
      of GN at all.
    
    For testing the output of `gn gen` has been compared before
    and after this CL was applied with the Fuchsia source tree
    to verify that the output is the same.
    
    On the other hand, if `no_stamp_files = true` is added
    to the `.gn` file, the generated Ninja file go from
    829 MiB to 797 MiB.
    
    Bug: 172
    Change-Id: If26e64c759ed34467ce13b5c5c1005d56f435947

--- src/gn/target_unittest.cc
+++ src/gn/target_unittest.cc
@@ -799,6 +799,7 @@ TEST_F(TargetTest, LinkAndDepOutputs) {
   ASSERT_TRUE(target.OnResolved(&err));
 
   EXPECT_EQ("./liba.so", target.link_output_file().value());
+  ASSERT_TRUE(target.has_dependency_output_file());
   EXPECT_EQ("./liba.so.TOC", target.dependency_output_file().value());
 
   ASSERT_EQ(1u, target.runtime_outputs().size());
@@ -845,6 +846,7 @@ TEST_F(TargetTest, RuntimeOuputs) {
   ASSERT_TRUE(target.OnResolved(&err));
 
   EXPECT_EQ("./a.dll.lib", target.link_output_file().value());
+  ASSERT_TRUE(target.has_dependency_output_file());
   EXPECT_EQ("./a.dll.lib", target.dependency_output_file().value());
 
   ASSERT_EQ(2u, target.runtime_outputs().size());
@@ -875,6 +877,7 @@ TEST_F(TargetTest, GetOutputFilesForSource_Binary) {
 
   Target target(setup.settings(), Label(SourceDir("//a/"), "a"));
   target.set_output_type(Target::SOURCE_SET);
+  target.sources().push_back(SourceFile("//a/source_file1.cc"));
   target.SetToolchain(&toolchain);
   Err err;
   ASSERT_TRUE(target.OnResolved(&err));
