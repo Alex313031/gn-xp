3bfe32252893cc6bb9342594cb7ff7c54dce3d57
David 'Digit' Turner <digit@google.com>
2022-03-26 18:08:44 +0100

    Add ImmutableVector and ImmutableVectorView templates.
    These templates allow creating and using heap-allocated but
    immutable arrays of items. They will be used by future CLs
    in a few key classes to reduce GN RAM usage drastically.
    
    Here's how an ImmutableVector compares to an std::vector<>
    in terms of memory usage:
    
    - An empty instance is just an 8-byte null pointer, vs 24 bytes
      for an empty vector.
    
    - Its heap block is sized precisely to fit the instance's size,
      while the std::vector<> will over-allocate its storage space
      (calling reserve() does not guarantee minimal heap usage).
    
    Note that constructing an non-empty ImmutableVector requires a
    source container (std::vector<> or otherwise), or a callable
    that can produce a fixed number of items for in-place
    construction (see source code for details).
    
    An ImmutableVectorView is a non-owning reference to an
    ImmutableVectorView, it is copyable and movable since the
    vector is immutable.
    
    + Add base::aligned_alloc() and base::aligned_free() to
      work around the lack fo std::aligned_alloc() on Windows
      and MacOS :-(
    
    Bug: None
    
    Change-Id: I5bb69553fcff8ef7b0f2a42ca7239bd967ecd9ce

---
+++ src/base/memory/aligned_alloc.h
@@ -0,0 +1,42 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_ALIGNED_ALLOC_H_
+#define BASE_ALIGNED_ALLOC_H_
+
+namespace base {
+
+#ifdef _WIN32
+// The Microsoft compiler does not support std::aligned_alloc
+// but it provides _aligned_malloc and _aligned_free
+#include <malloc.h>
+static inline void* aligned_alloc(size_t alignment, size_t size) {
+  return _aligned_malloc(size, alignment);
+}
+
+static inline void aligned_free(void* block) {
+  _aligned_free(block);
+}
+
+#elif defined(__APPLE__)
+#include <cstddef>
+// On MacOS, std::aligned_alloc() is only available with the most
+// recent releases of the system only, so provide a fallback to
+// run the compiled code on any OS X release.
+void* aligned_alloc(size_t alignment, size_t size);
+void aligned_free(void* block);
+#else  // !_WIN32
+#include <cstdlib>
+static inline void* aligned_alloc(size_t alignment, size_t size) {
+  return std::aligned_alloc(alignment, size);
+}
+
+static inline void aligned_free(void* block) {
+  std::free(block);
+}
+#endif  // !_WIN32
+
+}  // namespace base
+
+#endif  // BASE_ALIGNED_ALLOC_H_
