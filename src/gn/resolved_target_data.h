5c5ba3ea8a96f4d7ccc1805bad706ec63751458c
David 'Digit' Turner <digit@google.com>
2022-04-13 14:49:50 +0200

    Add ResolvedTargetData
    This class re-implements some of the expensive recursive computations
    over the build graph that are currently implemented in
    Target::OnResolved(). This will allow, in a future CL, to move these
    computations out of the graph builder, that runs exclusively on the
    main thread, and into parallel worked pool threads.
    
    Another benefit is that the computations are performed top-down
    (i.e. by recursing from dependents to dependencies), which limits
    them to only the targets that actually need them. By contrast, the
    current code propagates values from dependencies to dependents
    unconditionally, resulting in un-necessary work and data associated
    for _every_ target in the graph.
    
    To avoid duplicate work, each ResolvedTargetData call does record or
    reuses intermediate target-specific values. This will be leveraged,
    in yet another future CL, by setting up and using one
    ResolvedTargetData instance per worker pool thread.
    
    Future CLs will update the Ninja target writers to use instances of
    this class, and remove the Target methods that return the same results.
    
    + Add ResolvedTargetDeps which provides a compact iterator over
      different sets of target dependencies, since profiling shows
      that using a DepsIterator instance is much slower during
      graph-traversal heavy operations.
    
    Bug: None
    Change-Id: I83f2e3aed83e0ac1b96bb887e7f3f3f9a01a7156

---
+++ src/gn/resolved_target_data.h
@@ -0,0 +1,92 @@
+
+#ifndef TOOLS_GN_RESOLVED_TARGET_DATA_H_
+#define TOOLS_GN_RESOLVED_TARGET_DATA_H_
+
+#include "gn/immutable_vector.h"
+#include "gn/tagged_pointer.h"
+#include "gn/target.h"
+
+#include <memory>
+
+class Target;
+
+// A Compact encoding for a (target_ptr, is_public_flag) pair.
+class TargetPublicPair {
+ public:
+  TargetPublicPair() = default;
+  TargetPublicPair(const Target* target, bool is_public)
+      : pair_(target, static_cast<unsigned>(is_public)) {}
+  TargetPublicPair(std::pair<const Target*, bool> pair)
+      : pair_(pair.first, static_cast<unsigned>(pair.second)) {}
+
+  const Target* target() const { return pair_.ptr(); }
+  void set_target(const Target* target) { pair_.set_ptr(target); }
+
+  bool is_public() const { return pair_.tag() != 0; }
+  void set_is_public(bool is_public) { pair_.set_tag(is_public ? 1 : 0); }
+
+ private:
+  TaggedPointer<const Target, 1> pair_;
+};
+
+// A list of (target_ptr, is_public_flag) pairs as returned by methods
+// of ResolvedTargetData.
+using TargetPublicPairList = ImmutableVectorView<TargetPublicPair>;
+
+// A class used to compute data.
+class ResolvedTargetData {
+ public:
+  ResolvedTargetData();
+  ~ResolvedTargetData();
+
+  // Move operations
+  ResolvedTargetData(ResolvedTargetData&&) noexcept;
+  ResolvedTargetData& operator=(ResolvedTargetData&&);
+
+  // Retrieve information about link-time libraries needed by this target.
+  struct LibInfo {
+    ImmutableVectorView<SourceDir> all_lib_dirs;
+    ImmutableVectorView<LibFile> all_libs;
+  };
+  LibInfo GetLibInfo(const Target*) const;
+
+  ImmutableVectorView<SourceDir> all_lib_dirs(const Target* target) const;
+
+  ImmutableVectorView<LibFile> all_libs(const Target* target) const;
+
+  // Retrieve information about link-time OS X frameworks needed by this target.
+  struct FrameworkInfo {
+    ImmutableVector<SourceDir> all_framework_dirs;
+    ImmutableVector<std::string> all_frameworks;
+    ImmutableVector<std::string> all_weak_frameworks;
+  };
+  FrameworkInfo GetFrameworkInfo(const Target* target) const;
+
+  ImmutableVectorView<SourceDir> all_framework_dirs(const Target* target) const;
+
+  ImmutableVectorView<std::string> all_frameworks(const Target* target) const;
+
+  ImmutableVectorView<std::string> all_weak_frameworks(
+      const Target* target) const;
+
+  // Retrieve a set of hard dependencies for this target.
+  TargetSet recursive_hard_deps(const Target* target) const;
+
+  // Retrieve an ordered list of (target, is_public) pairs for all link-time
+  // libraries inherited by this target.
+  TargetPublicPairList inherited_libraries(const Target* target) const;
+
+  // Retrieves an ordered list of (target, is_public) paris for all link-time
+  // libraries for Rust-specific binary targets.
+  TargetPublicPairList rust_transitive_inherited_libs(
+      const Target* target) const;
+
+ private:
+  class Impl;
+
+  Impl* GetImpl() const;
+
+  mutable std::unique_ptr<Impl> impl_;
+};
+
+#endif  // TOOLS_GN_RESOLVED_TARGET_DATA_H_
