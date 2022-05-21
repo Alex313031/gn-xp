ddeb944691e20e011dffa42e41cd9420acff2360
Aaron Wood <aaronwood@google.com>
2022-05-20 18:33:57 -0700

    [template] Correct print_stack_trace() output and add toolchain
    print_stack_trace() was walking up both invoker and containing
    scopes, causing it to double-print entries.
    
    Instead, it needed to only walk up the invocation scopes, and
    for each invocation scope, search it and its containing scopes
    to find a template invocation entry.
    
    Change-Id: I7a2228b50f0dae0f4d8abe58f2f24e78bf2ea1d8

--- src/gn/functions.cc
+++ src/gn/functions.cc
@@ -21,7 +21,6 @@
 #include "gn/pool.h"
 #include "gn/scheduler.h"
 #include "gn/scope.h"
-#include "gn/settings.h"
 #include "gn/template.h"
 #include "gn/token.h"
 #include "gn/value.h"
@@ -1007,7 +1006,10 @@ Value RunPrintStackTrace(Scope* scope,
                          const std::vector<Value>& args,
                          Err* err) {
   std::string location_str = function->GetRange().begin().Describe(false);
-  std::string output = "print_stack_trace() initiated at:  " + location_str;
+  std::string toolchain =
+      scope->settings()->toolchain_label().GetUserVisibleName(false);
+  std::string output =
+      "print_stack_trace() initiated at:  " + location_str + "  using: " + toolchain;
   output.push_back('\n');
 
   for (const auto& entry : scope->GetTemplateInvocationEntries()) {
