timniederhausen/gn

commit fd6cae41bd7d5d255dc2fb96004a8bf74ac9d972

Avoid out-of-bounds string indexing
In GetSubBuildDirAsOutputFile(), when the source directory is the same
as the build directory, an out-of-bounds string index is used and thus
triggers an assertion in libstdc++. Fix this by checking if the source
directory is longer than the build directory before using the latter's
length as the index.

Bug: 293
Fixes: fd9f2036f26d ("Fix output directories for generated source
                      files.")
Change-Id: I0b0756a01f181fb14f4bd97b45e1f441be3c0e8b
Reviewed-on: https://gn-review.googlesource.com/c/gn/+/14080
Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>

src/gn/filesystem_utils.cc
@@ -1036,8 +1036,10 @@ OutputFile GetSubBuildDirAsOutputFile(const BuildDirContext& context,
       // or `toolchain2/obj/BUILD_DIR/toolchain1/gen` which look surprising,
       // but guarantee unicity.
       result.value().append("BUILD_DIR/");
-      result.value().append(&source_dir_path[build_dir.size()],
-                            source_dir_path.size() - build_dir.size());
+      if (source_dir_path.size() > build_dir.size()) {
+        result.value().append(&source_dir_path[build_dir.size()],
+                              source_dir_path.size() - build_dir.size());
+      }
     } else {
       // The source dir is source-absolute, so we trim off the two leading
       // slashes to append to the toolchain object directory.

Alex313031/gn-xp

commit 98f824642ae0c57f0c7d9afc29d50fb530eacc70

Simplify source_dir_path substring extraction
Instead of checking if 'source_dir_path' is longer than 'build_dir'
before using the latter's length as an index into the first string,
switch to calling the subsrt() method of the former. This correctly
handles the case of both strings having the same length while being
clearer about its intended function.

Bug: 293
Fixes: fd6cae41bd7d ("Avoid out-of-bounds string indexing")
Change-Id: I46d29efb579b70eec47e32004347ebfccc56430e

src/gn/filesystem_utils.cc
@@ -1036,10 +1036,8 @@ OutputFile GetSubBuildDirAsOutputFile(const BuildDirContext& context,
       // or `toolchain2/obj/BUILD_DIR/toolchain1/gen` which look surprising,
       // but guarantee unicity.
       result.value().append("BUILD_DIR/");
-      if (source_dir_path.size() > build_dir.size()) {
-        result.value().append(&source_dir_path[build_dir.size()],
-                              source_dir_path.size() - build_dir.size());
-      }
+      result.value().append(source_dir_path.substr(build_dir.size()));
+
     } else {
       // The source dir is source-absolute, so we trim off the two leading
       // slashes to append to the toolchain object directory.