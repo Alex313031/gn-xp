395cd94b9fbee318a7ee57afa80ce18f05f72718
Brett Wilson <brettw@chromium.org>
2022-08-31 15:49:44 -0700

    Add export compile commands .gn setting.
    Adds a "export_compile_commands" setting to the .gn file. This can be
    used as a consistent way to generate a compilation database for projects
    that require this (the existing --export-compile-commands switch is
    intended for end-users to use with editor integration).
    
    The new variable uses label patterns for better consistency with the
    rest of the system. The switch value takes precendence if present.
    
    Bug: 111
    
    Change-Id: Ieec41f55e9a140066e8b3e3f69115ab087f52172

--- src/gn/compile_commands_writer.h
+++ src/gn/compile_commands_writer.h
@@ -5,7 +5,10 @@
 #ifndef TOOLS_GN_COMPILE_COMMANDS_WRITER_H_
 #define TOOLS_GN_COMPILE_COMMANDS_WRITER_H_
 
+#include <vector>
+
 #include "gn/err.h"
+#include "gn/label_pattern.h"
 #include "gn/target.h"
 
 class Builder;
@@ -13,23 +16,63 @@ class BuildSettings;
 
 class CompileCommandsWriter {
  public:
-  // Write compile commands into a json file located by parameter file_name.
+  // Encapsulates the different ways of specifying which targets to generate for
+  // compile commands.
+  // 
+  // The original way to specify this was the command-line switch which matched
+  // targets in any directory with a given taget_name.
   //
-  // Parameter target_filters should be in "target_name1,target_name2..."
-  // format. If it is not empty, only targets that are reachable from targets
-  // in target_filters are used to generate compile commands.
+  // Later, a setting in the .gn file was added and this was converted to label
+  // patterns for consistency with other GN designs. If the command-line switch
+  // is specified, it takes precedence.
   //
-  // Parameter quiet is not used.
+  // TODO remove the legacy target_name behavior and use vector<LabelPattern>
+  // consistently. This struct can then be removed.
+  struct Filter {
+    explicit Filter(std::string legacy_filters)
+        : mode(kLegacy), legacy_target_filters(std::move(legacy_filters)) {}
+    explicit Filter(std::vector<LabelPattern> patterns)
+        : mode(kLabelPattern), patterns(std::move(patterns)) {}
+
+    enum Mode { kLegacy, kLabelPattern };
+    Mode mode;
+
+    std::string legacy_target_filters;   // When mode == kLegacy.
+    std::vector<LabelPattern> patterns;  // When mode == kLabelPattern.
+  };
+
+  // Writes a compilation databased to the given file name consisting of the
+  // recursive dependencies of all targets that match or are dependencies of
+  // targets that match any given pattern.
   static bool RunAndWriteFiles(const BuildSettings* build_setting,
                                const Builder& builder,
-                               const std::string& file_name,
+                               const base::FilePath& output_path,
+                               const std::vector<LabelPattern>& patterns,
+                               Err* err);
+
+  // Writes a compilation database using the legacy way of specifying which
+  // targets to output. This format uses a comma-separated list of target names
+  // ("target_name1,target_name2...") which are matched against targets in any
+  // directory. Then the recursive dependencies of these deps are collected.
+  //
+  // TODO remove this legacy target_name behavior and use vector<LabelPattern>
+  // version consistently.
+  static bool RunAndWriteFilesLegacyFilters(const BuildSettings* build_setting,
+                               const Builder& builder,
+                               const base::FilePath& output_path,
                                const std::string& target_filters,
-                               bool quiet,
                                Err* err);
 
   static std::string RenderJSON(const BuildSettings* build_settings,
                                 std::vector<const Target*>& all_targets);
 
+  // Does a depth-first search of the graph starting at each target that matches
+  // the given pattern, and collects all recursive dependencies of those targets.
+  static std::vector<const Target*> CollectDepsOfMatches(
+      const std::vector<const Target*>& all_targets,
+      const std::vector<LabelPattern>& patterns);
+
+  // Performs the legacy target_name filtering.
   static std::vector<const Target*> FilterTargets(
       const std::vector<const Target*>& all_targets,
       const std::set<std::string>& target_filters_set);
