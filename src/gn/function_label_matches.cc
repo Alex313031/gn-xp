eb889ebce4592e5a5d55986272f548e189ecf8e0
Aaron Wood <aaronwood@google.com>
2024-02-14 13:40:53 -0800

    [label_matches] Add new functions label_matches(), filter_labels_include() and filter_labels_exclude()
    Add a new function 'label_matches(target_label, label_patterns)' that
    can be used to test if a given GN label matches any of the given label
    patterns.
    
    Add a new pair of functions:
     - 'filter_labels_include(labels, label_patterns)'
     - 'filter_labels_exclude(labels, label_patterns)'
    
    These returns all labels in the first list that match (or don't match,
    respectively) any of the patterns in the second list (much like the
    'filter_include()' and 'filter_exclude()' functions do for files.
    
    Change-Id: I7670099dce41c3ff38219adf8fd740b6fb935e78

---
+++ src/gn/function_label_matches.cc
@@ -0,0 +1,197 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+
+#include "gn/build_settings.h"
+#include "gn/err.h"
+#include "gn/functions.h"
+#include "gn/label_pattern.h"
+#include "gn/parse_tree.h"
+#include "gn/scope.h"
+#include "gn/settings.h"
+#include "gn/value.h"
+
+namespace functions {
+
+const char kLabelMatches[] = "label_matches";
+const char kLabelMatches_HelpShort[] =
+    "label_matches: Returns whether a label matches any of a list of patterns.";
+const char kLabelMatches_Help[] =
+    R"(label_matches: Returns true if the label matches any of a set of patterns.
+
+  label_matches(target_label, patterns)
+
+  The argument patterns must be a list of label patterns (see
+  "gn help label_pattern"). If the target_label matches any of the patterns,
+  the function returns the value true.
+
+Examples
+  result = label_matches("//baz:bar", [ "//foo/bar/*", "//baz:*" ])
+  # result will be true
+)";
+
+Value RunLabelMatches(Scope* scope,
+                      const FunctionCallNode* function,
+                      const std::vector<Value>& args,
+                      Err* err) {
+  if (args.size() != 2) {
+    *err = Err(function, "Expecting exactly two arguments.");
+    return Value();
+  }
+
+  // Extract "values".
+  if (args[0].type() != Value::STRING) {
+    *err = Err(args[0], "First argument must be a target label.");
+    return Value();
+  }
+
+  // Extract "patterns".
+  std::vector<LabelPattern> patterns;
+  for (const auto& value : args[1].list_value()) {
+    if (value.type() != Value::STRING) {
+      *err = Err(args[1], "Second argument must be a list of label patterns");
+      return Value();
+    }
+    LabelPattern pattern = LabelPattern::GetPattern(
+        scope->GetSourceDir(),
+        scope->settings()->build_settings()->root_path_utf8(), value, err);
+    if (err->has_error()) {
+      return Value();
+    }
+    patterns.push_back(pattern);
+  }
+
+  Value result(function, Value::BOOLEAN);
+  Label label =
+      Label::Resolve(scope->GetSourceDir(),
+                     scope->settings()->build_settings()->root_path_utf8(),
+                     ToolchainLabelForScope(scope), args[0], err);
+  if (label.is_null()) {
+    return Value();
+  }
+
+  const bool matches_pattern = LabelPattern::VectorMatches(patterns, label);
+  if (matches_pattern) {
+    result.boolean_value() = true;
+  }
+  return result;
+}
+
+enum FilterSelection {
+  kExcludeFilter,
+  kIncludeFilter,
+};
+
+Value RunFilterLabels(Scope* scope,
+                      const FunctionCallNode* function,
+                      const std::vector<Value>& args,
+                      FilterSelection selection,
+                      Err* err) {
+  if (args.size() != 2) {
+    *err = Err(function, "Expecting exactly two arguments.");
+    return Value();
+  }
+
+  // Extract "values".
+  if (args[0].type() != Value::LIST) {
+    *err = Err(args[0], "First argument must be a list of target labels.");
+    return Value();
+  }
+
+  // Extract "patterns".
+  std::vector<LabelPattern> patterns;
+  for (const auto& value : args[1].list_value()) {
+    if (value.type() != Value::STRING) {
+      *err = Err(args[1], "Second argument must be a list of label patterns");
+      return Value();
+    }
+    LabelPattern pattern = LabelPattern::GetPattern(
+        scope->GetSourceDir(),
+        scope->settings()->build_settings()->root_path_utf8(), value, err);
+    if (err->has_error()) {
+      return Value();
+    }
+    patterns.push_back(pattern);
+  }
+
+  Value result(function, Value::LIST);
+  for (const auto& value : args[0].list_value()) {
+    Label label =
+        Label::Resolve(scope->GetSourceDir(),
+                       scope->settings()->build_settings()->root_path_utf8(),
+                       ToolchainLabelForScope(scope), value, err);
+    if (label.is_null())
+      return Value();
+    const bool matches_pattern = LabelPattern::VectorMatches(patterns, label);
+    switch (selection) {
+      case kIncludeFilter:
+        if (matches_pattern)
+          result.list_value().push_back(value);
+        break;
+
+      case kExcludeFilter:
+        if (!matches_pattern)
+          result.list_value().push_back(value);
+        break;
+    }
+  }
+  return result;
+}
+
+const char kFilterLabelsInclude[] = "filter_labels_include";
+const char kFilterLabelsInclude_HelpShort[] =
+    "filter_labels_include: Remove labels that do not match a set of patterns.";
+const char kFilterLabelsInclude_Help[] =
+    R"(filter_labels_include: Remove labels that do not match a set of patterns.
+
+  filter_labels_include(labels, include_patterns)
+
+  The argument labels must be a list of strings.
+
+  The argument include_patterns must be a list of label patterns (see
+  "gn help label_pattern"). Only elements from labels matching at least
+  one of the patterns will be included.
+
+Examples
+  labels = [ "//foo:baz", "//foo/bar:baz", "//bar:baz" ]
+  result = filter_labels_include(labels, [ "//foo:*" ])
+  # result will be [ "//foo:baz" ]
+)";
+
+Value RunFilterLabelsInclude(Scope* scope,
+                             const FunctionCallNode* function,
+                             const std::vector<Value>& args,
+                             Err* err) {
+  return RunFilterLabels(scope, function, args, kIncludeFilter, err);
+}
+
+const char kFilterLabelsExclude[] = "filter_labels_exclude";
+const char kFilterLabelsExclude_HelpShort[] =
+    "filter_labels_exclude: Remove labels that match a set of patterns.";
+const char kFilterLabelsExclude_Help[] =
+    R"(filter_labels_exclude: Remove labels that match a set of patterns.
+
+  filter_labels_exclude(labels, exclude_patterns)
+
+  The argument labels must be a list of strings.
+
+  The argument exclude_patterns must be a list of label patterns (see
+  "gn help label_pattern"). Only elements from labels matching at least
+  one of the patterns will be excluded.
+
+Examples
+  labels = [ "//foo:baz", "//foo/bar:baz", "//bar:baz" ]
+  result = filter_labels_exclude(labels, [ "//foo:*" ])
+  # result will be [ "//foo/bar:baz", "//bar:baz" ]
+)";
+
+Value RunFilterLabelsExclude(Scope* scope,
+                             const FunctionCallNode* function,
+                             const std::vector<Value>& args,
+                             Err* err) {
+  return RunFilterLabels(scope, function, args, kExcludeFilter, err);
+}
+
+}  // namespace functions
